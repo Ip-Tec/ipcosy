7966d68b7e7dc28d4c9f52b89e23e36874bc3b3d
Ip-Tec <otakhorpeter@gmail.com>
2026-01-11 23:43:56 +0100

    Error fix

--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -1,6 +1,6 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "@ipcosy/db";
 import { nanoid } from "nanoid";
 import { cookies } from "next/headers";
